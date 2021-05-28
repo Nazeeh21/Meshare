7ed038a8629f4d93118f21fa914cd487a33c2964
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-05-28 19:28:53 +0530

    add initial setup for github auth

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -4,6 +4,8 @@ declare namespace NodeJS {
     DB_NAME: string;
     DB_USERNAME: string;
     DB_PASSWORD: string;
+    GITHUB_CLIENT_SECRET: string;
+    GITHUB_CLIENT_ID: string;
     CORS_ORIGIN: string;
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,9 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { QuestionResolver } from './resolvers/question';
 import { MyContext } from './types';
+import passport from 'passport';
+import { Strategy as GitHubStrategy } from 'passport-github';
+
 
 const main = async () => {
   // command for generating tables: npx typeorm migration:generate -n Initial
@@ -39,6 +42,8 @@ const main = async () => {
     })
   );
 
+  app.use(passport.initialize());
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [QuestionResolver],
@@ -54,6 +59,50 @@ const main = async () => {
     app,
     cors: false,
   });
+
+  passport.serializeUser((user: any, done) => {
+    done(null, user.accessToken);
+  });
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        // @ts-ignore
+        clientID: process.env.GITHUB_CLIENT_ID,
+        // @ts-ignore
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'http://localhost:4000/auth/github/callback',
+      },
+      async (_: any, __: any, profile: any, cb: any) => {
+       console.log(profile);
+       
+
+        cb(null, { accessToken: '', refreshToken: ''
+          // accessToken: jwt.sign(
+          //   { userId: user.id },
+          //   // @ts-ignore
+          //   process.env.ACCESS_TOKEN_SECRET,
+          //   // 'sdhbjshj',
+          //   {
+          //     expiresIn: '1y',
+          //   }
+          // ),
+        });
+      }
+    )
+  );
+
+  app.get('/auth/github', passport.authenticate('github', { session: false }));
+
+  app.get(
+    '/auth/github/callback',
+    passport.authenticate('github', { session: false, failureRedirect: '/' }),
+    (_req: any, res) => {
+      // Successful authentication, redirect home.
+      // res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`);
+      res.send('auth was successful')
+    }
+  );
 
   app.listen(+process.env.PORT, () => {
     console.log('server started on port localhost:4000');
