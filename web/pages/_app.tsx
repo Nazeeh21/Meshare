6efeb517ca934590afbda92171afd0e5376e16e2
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-05-29 22:32:56 +0530

    add the colors globally in chakra ui

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,26 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import 'tailwindcss/tailwind.css'
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "tailwindcss/tailwind.css";
+
+const theme = extendTheme({
+  styles: {
+    colors: {
+      brand: {
+        100: "#1C3F73",
+        200: "#E0EAF1",
+        300: "#1389E6",
+        400: "#F5FAFE",
+        500: "#BCD2E2",
+        600: "#4E6072",
+      },
+    },
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
