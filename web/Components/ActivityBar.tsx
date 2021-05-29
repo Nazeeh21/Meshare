edc4ff09cab0546f1dce0870114cd19ec3b46c0a
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-05-30 01:14:33 +0530

    add the image part in sidebar

--- web/Components/ActivityBar.tsx
+++ web/Components/ActivityBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const ActivityBar: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-sidebarH w-3/12 rounded-r-md bg-activityBlue">
+    <div className="h-sidebarH w-3/12 lg:w-4/12 md:w-6/12 rounded-r-md bg-activityBlue">
       {children}
     </div>
   );
--- web/Components/Sidebar.tsx
+++ web/Components/Sidebar.tsx
@@ -1,8 +1,19 @@
 import { LayoutProps } from "framer-motion";
 import React from "react";
-
-export const Sidebar: React.FC<LayoutProps> = ({ children }) => {
+interface Props {
+  image: string;
+}
+export const Sidebar: React.FC<Props> = ({ image }: Props) => {
   return (
-    <div className="h-sidebarH bg-white w-24 rounded-l-md">{children}</div>
+    <div className="h-sidebarH lg:w-32 md:w-32 bg-white w-1/12 rounded-l-md flex flex-col">
+      <div className="h-16 bg-iconBlue w-16 rounded-full mx-auto mt-8 overflow-hidden">
+        <img src="/miral.jpg" alt="" />
+      </div>
+      <div>
+        <div>
+          <img src="/browse.png" alt="" />
+        </div>
+      </div>
+    </div>
   );
 };
