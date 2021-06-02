9d3c2bd8a71a292aee5c13fc2635b69efe81305e
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-06-02 20:58:33 +0530

    Add list of eventTypes and payloads from github

--- web/Components/Sidebar.tsx
+++ web/Components/Sidebar.tsx
@@ -1,17 +1,17 @@
-import { LayoutProps } from "framer-motion";
-import React, { useEffect, useState } from "react";
+import { LayoutProps } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 import {
   faGlobe,
   faQuestionCircle,
   faPenNib,
   faSignOutAlt,
   // @ts-ignore
-} from "@fortawesome/free-solid-svg-icons";
-import { SidebarButton } from "./SidebarButton";
-import { useGetUserQuery, useLogoutMutation } from "../generated/graphql";
-import { isServer } from "../utils/isServer";
-import { useRouter } from "next/router";
-import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
+} from '@fortawesome/free-solid-svg-icons';
+import { SidebarButton } from './SidebarButton';
+import { useGetUserQuery, useLogoutMutation } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 interface Props {
   image?: string;
 }
@@ -23,41 +23,44 @@ export const Sidebar: React.FC<Props> = ({ image }: Props) => {
 
   const userData = useSelector((state: RootStateOrAny) => state.main.userData);
   useEffect(() => {
-    console.log("userData: ", userData);
+    console.log('userData: ', userData);
   }, [userData]);
 
   return (
-    <div className="h-sidebarH lg:w-32 md:w-32 bg-white w-1/12 rounded-l-md hidden md:flex flex-col justify-around">
-      <div
-        onClick={() => router.push(`https://www.github.com/${userData.name}`)}
-        className="h-16 bg-iconBlue w-16 rounded-full mx-auto mt-8 overflow-hidden"
-      >
+    <div className='h-sidebarH lg:w-32 md:w-32 bg-white w-1/12 rounded-l-md hidden md:flex flex-col justify-around'>
+      <div className='h-16 bg-iconBlue w-16 rounded-full mx-auto mt-8 overflow-hidden'>
         <img
-          src={userData ? userData.avatarUrl : "/doraemon.svg"}
-          alt={userData ? userData.name : "Default avatar"}
+          onClick={() => {
+            if (userData) {
+              router.push(`https://www.github.com/${userData.name}`);
+            }
+          }}
+          className={userData && 'cursor-pointer'}
+          src={userData ? userData.avatarUrl : '/doraemon.svg'}
+          alt={userData ? userData.name : 'Default avatar'}
         />
       </div>
       <div>
-        <div className="my-24">
+        <div className='my-24'>
           <SidebarButton
             click={() => setActive(0)}
             isActive={active === 0}
             icon={faGlobe}
-            size={"2x"}
+            size={'2x'}
           />
 
           <SidebarButton
             click={() => setActive(1)}
             isActive={active === 1}
             icon={faQuestionCircle}
-            size={"2x"}
+            size={'2x'}
           />
 
           <SidebarButton
             click={() => setActive(2)}
             isActive={active === 2}
             icon={faPenNib}
-            size={"2x"}
+            size={'2x'}
           />
         </div>
       </div>
@@ -69,8 +72,8 @@ export const Sidebar: React.FC<Props> = ({ image }: Props) => {
             router.reload();
           }}
           icon={faSignOutAlt}
-          color={"#BCD2E2"}
-          size={"2x"}
+          color={'#BCD2E2'}
+          size={'2x'}
         />
         {/* </Button> */}
       </div>
