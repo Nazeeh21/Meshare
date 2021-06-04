93de560b234620542f4dfa524fe3110c4e908277
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-06-04 21:15:56 +0530

    Add upload image to supabase

---
+++ web/pages/upload.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../utils/supabaseClient';
+
+const upload: React.FC = ({}) => {
+  const [files, setFiles] = useState(null);
+  useEffect(() => {
+    if (files) {
+      console.log(files);
+      console.log('relative url: ', URL.createObjectURL(files));
+    }
+  }, [files]);
+  
+  const onUploadClick = async () => {
+    const { data, error } = await supabase.storage
+      .from('avatars')
+      .upload(files.name, files);
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  };
+
+  return (
+    <div>
+      <input type='file' onChange={(e) => setFiles(e.target.files[0])} />
+      <button onClick={onUploadClick}>Upload</button>
+      {files && (
+        <img className='w-20 h-20' src={URL.createObjectURL(files)} alt='' />
+      )}
+    </div>
+  );
+};
+
+export default upload;
