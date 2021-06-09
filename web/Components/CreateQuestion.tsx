6d7a16cf785fd76862079b6b104a932b3dd981b0
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-06-09 23:04:38 +0530

    add styling to upload button and add cancel button ti image

--- web/Components/CreateQuestion.tsx
+++ web/Components/CreateQuestion.tsx
@@ -1,27 +1,24 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Autosuggest from 'react-autosuggest';
-import TagsInput from 'react-tagsinput';
-import UploadComponent from './UploadComponent';
+import React, { useEffect, useState, useRef } from "react";
+import Autosuggest from "react-autosuggest";
+import TagsInput from "react-tagsinput";
+import UploadComponent from "./UploadComponent";
 
 export const CreateQuestion = () => {
   const [tags, setTags] = useState([]);
-  const suggestions = [
-    { name: 'react' },
-    { name: 'react-native' },
-  ];
+  const suggestions = [{ name: "react" }, { name: "react-native" }];
   const onChange = (tag) => {
     setTags(tag);
   };
   function autosuggestRenderInput({ addTag, ...props }) {
     const handleOnChange = (e, { newValue, method }) => {
-      if (method === 'enter') {
+      if (method === "enter") {
         e.preventDefault();
       } else {
         props.onChange(e);
       }
     };
 
-    const inputValue = (props.value && props.value.trim().toLowerCase()) || '';
+    const inputValue = (props.value && props.value.trim().toLowerCase()) || "";
     const inputLength = inputValue.length;
 
     let suggestion = suggestions.filter((state) => {
@@ -47,29 +44,31 @@ export const CreateQuestion = () => {
 
   return (
     <div>
-      <div className='h-screen'>
-        <div className='w-full min-h-24 mb-5 bg-iconGrey rounded-md'>
+      <div className="h-screen">
+        <div className="w-full min-h-24 mb-5 bg-iconGrey rounded-md">
           <textarea
-            className='w-full h-16 text-black bg-iconGrey outline-none pt-2 px-2'
-            placeholder='write your question here..'
+            className="w-full h-16 rounded-md text-black bg-iconGrey outline-none pt-2 px-2"
+            placeholder="write your question here.."
           />
           <UploadComponent />
-          {/* <button className='bg-submitButton py-0 px-3 float-right rounded-md text-white font-bold'>
-            upload
-          </button> */}
         </div>
         <TagsInput
+          className="mt-10 rounded-md w-full bg-iconGrey"
           renderInput={autosuggestRenderInput}
           value={tags}
           onChange={(e) => onChange(e)}
           maxTags={3}
           tagProps={{
-            className: 'bg-activityBlue text-white react-tagsinput-tag text-md font-medium rounded-md ml-2',
-            classNameRemove: 'react-tagsinput-remove'
+            className:
+              "bg-activityBlue text-white react-tagsinput-tag text-md font-medium rounded-md ml-2",
+            classNameRemove: "react-tagsinput-remove",
           }}
         />
-        
-        <button className='mt-6 bg-submitButton py-2 px-3 rounded-md outline-none text-lg font-bold text-white'>
+
+        <button
+          onClick={() => {}}
+          className="mt-6 bg-submitButton py-2 px-3 rounded-md outline-none text-lg font-bold text-white"
+        >
           SUBMIT
         </button>
       </div>
