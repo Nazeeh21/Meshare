91c063476af6d38ebc3a10d078153a40e2926e25
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-06-15 12:28:34 +0530

    Add /login route

---
+++ web/pages/login.tsx
@@ -0,0 +1,54 @@
+import { RootStateOrAny, useSelector } from 'react-redux';
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useGetUserQuery } from '../generated/graphql';
+import { useEffect } from 'react';
+import { isServer } from '../utils/isServer';
+
+const login = () => {
+  const router = useRouter();
+  const userData = useSelector((state: RootStateOrAny) => state.main.userData);
+  const isLoggedIn = useSelector(
+    (state: RootStateOrAny) => state.main.isLoggedIn
+  );
+  const [{ data, fetching }] = useGetUserQuery({
+    pause: isServer(),
+  });
+
+  useEffect(() => {
+    if (!fetching && data?.getUser) {
+      router.push('/');
+    }
+  }, [data, fetching]);
+
+  if (userData && isLoggedIn) {
+    router.push('/');
+  }
+
+  if (!isLoggedIn && !userData) {
+    return (
+      <div
+        style={{
+          backgroundColor: '#4aa96c',
+          width: '12vw',
+          padding: '0.5rem',
+          borderRadius: '0.2rem',
+          fontWeight: 'bold',
+        }}
+        className='text-center mt-6 m-auto'
+      >
+        <a
+          href='http://localhost:4000/auth/github/'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          Login using github
+        </a>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default login;
