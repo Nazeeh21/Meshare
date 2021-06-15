8c45e20afc6e6c220d7f85862ea6a7a6b21f0125
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-06-15 13:10:37 +0530

    invalidate cache on createQuestion

--- web/utils/createUrqlClient.ts
+++ web/utils/createUrqlClient.ts
@@ -1,20 +1,141 @@
-import { dedupExchange,  Exchange, fetchExchange } from '@urql/core';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { dedupExchange, Exchange, fetchExchange } from '@urql/core';
+import { Cache, cacheExchange, Resolver } from '@urql/exchange-graphcache';
 import Router from 'next/router';
+import { stringifyVariables } from 'urql';
 import { pipe, tap } from 'wonka';
 import { isServer } from './isServer';
 
-const errorExchange: Exchange = ({ forward }) => (ops$) => {
-  return pipe(
-    forward(ops$),
-    tap(({ error }) => {
-      if (error?.message.includes('not Authenticated')) {
-        // console.log('error: ', error.message);
-        Router.replace('/login');
+const errorExchange: Exchange =
+  ({ forward }) =>
+  (ops$) => {
+    return pipe(
+      forward(ops$),
+      tap(({ error }) => {
+        if (error?.message.includes('not Authenticated')) {
+          // console.log('error: ', error.message);
+          Router.replace('/login');
+        }
+      })
+    );
+  };
+
+export type MergeMode = 'before' | 'after';
+
+export interface PaginationParams {
+  offsetArgument?: string;
+  limitArgument?: string;
+  mergeMode?: MergeMode;
+}
+
+const cursorPagination = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    const allFields = cache.inspectFields(entityKey);
+
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+
+    const isItInTheCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string,
+      'questions'
+    );
+
+    info.partial = !isItInTheCache;
+
+    const results: string[] = [];
+
+    let hasMore = true;
+
+    fieldInfos.forEach((fieldInfo) => {
+      const key = cache.resolve(entityKey, fieldInfo.fieldKey) as string;
+      const data = cache.resolve(key, 'questions') as string[];
+      const _hasMore = cache.resolve(key, 'hasMore');
+
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
       }
-    })
-  );
+
+      results.push(...data);
+    });
+    return {
+      __typename: 'PaginatedQuestions',
+      hasMore,
+      questions: results,
+    };
+  };
 };
+
+const cursorPaginationforComments = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    const allFields = cache.inspectFields(entityKey);
+
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+
+    const isItInTheCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string,
+      'comments'
+    );
+
+    info.partial = !isItInTheCache;
+
+    const results: string[] = [];
+
+    let hasMore = true;
+
+    fieldInfos.forEach((fieldInfo) => {
+      const key = cache.resolve(entityKey, fieldInfo.fieldKey) as string;
+      const data = cache.resolve(key, 'comments') as string[];
+      const _hasMore = cache.resolve(key, 'hasMore');
+
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
+      // const data = cache.resolve(entityKey, fieldInfo.fieldKey) as string[];
+      results.push(...data);
+    });
+
+    return {
+      __typename: 'PaginatedComments',
+      hasMore,
+      posts: results,
+    };
+  };
+};
+
+const invalidateAllQuestions = (cache: Cache) => {
+  const allFields = cache.inspectFields('Query');
+  const fieldInfos = allFields.filter((info) => info.fieldName === 'questions');
+
+  fieldInfos.forEach((fieldInfo) => {
+    cache.invalidate('Query', 'questions', fieldInfo.arguments || {});
+  });
+};
+
+const invalidateAllComments = (cache: Cache) => {
+  const allFields = cache.inspectFields('Query');
+  const fieldInfos = allFields.filter((info) => info.fieldName === 'comments');
+
+  fieldInfos.forEach((fieldInfo) => {
+    cache.invalidate('Query', 'comments', fieldInfo.arguments || {});
+  });
+};
+
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = '';
   if (isServer()) {
@@ -35,6 +156,26 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
       dedupExchange,
       cacheExchange({
         // TODO: add keys, resolvers, updates
+        keys: {
+          PaginatedQuestions: () => null,
+          PaginatedComments: () => null,
+        },
+        resolvers: {
+          Query: {
+            questions: cursorPagination(),
+            comments: cursorPaginationforComments(),
+          },
+        },
+        updates: {
+          Mutation: {
+            createComment: (_result, _args, cache, _info) => {
+              invalidateAllComments(cache);
+            },
+            createQuestion: (_result, _args, cache, _info) => {
+              invalidateAllQuestions(cache);
+            },
+          },
+        },
       }),
       errorExchange,
       ssrExchange,
