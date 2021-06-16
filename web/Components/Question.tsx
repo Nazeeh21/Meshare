aec37b395d4429c2fe6c738ba1ac3f66e65d3e86
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-06-16 14:52:05 +0530

    add the like and bookmark button

--- web/Components/Question.tsx
+++ web/Components/Question.tsx
@@ -1,6 +1,9 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import { Tag } from './Tags';
+import { useRouter } from "next/router";
+import React from "react";
+import { Tag } from "./Tags";
+import { Icon, InlineIcon } from "@iconify/react";
+import arrowUpThick from "@iconify/icons-akar-icons/arrow-up-thick";
+import bookmarkIcon from "@iconify/icons-bi/bookmark";
 
 interface QuestionProps {
   question: any;
@@ -9,37 +12,49 @@ interface QuestionProps {
 const Question: React.FC<QuestionProps> = ({ question }) => {
   const router = useRouter();
   return (
-    <div className='flex'>
+    <div className="flex">
       <img
-        className='w-10 h-10 rounded-full mr-2 cursor-pointer'
+        className="w-10 h-10 rounded-full mr-2 cursor-pointer"
         onClick={() =>
           router.push(`https://github.com/${question.creator.name}`)
         }
         src={question.creator.avatarUrl}
         alt={question.creator.name}
       />
-      <div className='w-full rounded-md bg-activityBlue text-white rounded-tl-none p-2 mb-4 pl-3'>
-        <div className='flex justify-space items-center mb-4'>
-          {router.pathname === '/' ? (
+      <div className="w-full rounded-md bg-activityBlue relative text-white rounded-tl-none p-2 mb-4 pl-3">
+        <div className="flex justify-space items-center mb-4">
+          {router.pathname === "/" ? (
             <div
-              className='cursor-pointer'
+              className="cursor-pointer"
               onClick={() => router.push(`/questions/${question.id}`)}
             >
-              {question.description.slice(0, 100)}{' '}
+              {question.description.slice(0, 100)}{" "}
               {question.description.length > 100 && <span>. . .</span>}
             </div>
           ) : (
             <div>{question.description}</div>
           )}
-          <div>{/* TODO: Like and unlike options */}</div>
+          <div className="absolute right-0 bottom-0 flex justify-center items-center p-2">
+            <Icon className="mr-3" height={30} icon={arrowUpThick} />
+            <h1>{/* {the amount of likes} */}</h1>
+            <Icon
+              className="ml-3"
+              height={30}
+              icon={arrowUpThick}
+              vFlip={true}
+            />
+          </div>
+          <div className="absolute right-0 top-0 p-3">
+            <Icon icon={bookmarkIcon} height={25} />
+          </div>
         </div>
         {question?.tags &&
           question?.tags.map((tag, index) => <Tag key={index} tag={tag} />)}
-        <div className='mt-2'>
-          Posted by:{' '}
+        <div className="mt-2">
+          Posted by:{" "}
           <a
             href={`https://github.com/${question.creator.name}`}
-            className='inline-block font-medium'
+            className="inline-block font-medium"
           >
             {question.creator.name}
           </a>
--- web/Components/Sidebar.tsx
+++ web/Components/Sidebar.tsx
@@ -1,17 +1,18 @@
-import { LayoutProps } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
+import { LayoutProps } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import {
   faGlobe,
   faQuestionCircle,
   faPenNib,
   faSignOutAlt,
+  faAddressBook,
   // @ts-ignore
-} from '@fortawesome/free-solid-svg-icons';
-import { SidebarButton } from './SidebarButton';
-import { useGetUserQuery, useLogoutMutation } from '../generated/graphql';
-import { isServer } from '../utils/isServer';
-import { useRouter } from 'next/router';
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+} from "@fortawesome/free-solid-svg-icons";
+import { SidebarButton } from "./SidebarButton";
+import { useGetUserQuery, useLogoutMutation } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
+import { useRouter } from "next/router";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 interface Props {
   image?: string;
 }
@@ -23,64 +24,75 @@ export const Sidebar: React.FC<Props> = ({ image }: Props) => {
 
   const userData = useSelector((state: RootStateOrAny) => state.main.userData);
   useEffect(() => {
-    console.log('userData: ', userData);
+    console.log("userData: ", userData);
   }, [userData]);
 
   useEffect(() => {
     const path = router.pathname;
 
-    if (path == '/create-question') {
+    if (path == "/create-question") {
       setActive(2);
-    } else if (path == '/my-questions') {
+    } else if (path == "/my-questions") {
       setActive(1);
-    } else if (path == '/') {
+    } else if (path == "/") {
       setActive(0);
+    } else if (path == "/bookmarked-question") {
+      setActive(3);
     } else {
       setActive(-1);
     }
   }, [router]);
 
   return (
-    <div className='h-sidebarH lg:w-32 md:w-32 bg-white w-1/12 rounded-l-md hidden md:flex flex-col justify-around'>
-      <div className='h-16 bg-iconBlue w-16 rounded-full mx-auto mt-8 overflow-hidden'>
+    <div className="h-sidebarH lg:w-32 md:w-32 bg-white w-1/12 rounded-l-md hidden md:flex flex-col justify-around">
+      <div className="h-16 bg-iconBlue w-16 rounded-full mx-auto mt-8 overflow-hidden">
         <img
           onClick={() => {
             if (userData) {
               router.push(`https://www.github.com/${userData.name}`);
             }
           }}
-          className={userData && 'cursor-pointer'}
-          src={userData ? userData.avatarUrl : '/doraemon.svg'}
-          alt={userData ? userData.name : 'Default avatar'}
+          className={userData && "cursor-pointer"}
+          src={userData ? userData.avatarUrl : "/doraemon.svg"}
+          alt={userData ? userData.name : "Default avatar"}
         />
       </div>
       <div>
-        <div className='my-24'>
+        <div className="my-24">
           <SidebarButton
             click={() => {
               setActive(0);
-              router.push('/');
+              router.push("/");
             }}
             isActive={active === 0}
             icon={faGlobe}
-            size={'2x'}
+            size={"2x"}
           />
 
           <SidebarButton
             click={() => setActive(1)}
             isActive={active === 1}
             icon={faQuestionCircle}
-            size={'2x'}
+            size={"2x"}
           />
 
           <SidebarButton
             click={() => {
               setActive(2);
-              router.push('/create-question');
+              router.push("/create-question");
             }}
             isActive={active === 2}
             icon={faPenNib}
-            size={'2x'}
+            size={"2x"}
+          />
+          <SidebarButton
+            click={() => {
+              setActive(3);
+              router.push("/bookmarked-question");
+            }}
+            isActive={active === 3}
+            icon={faAddressBook}
+            size={"2x"}
           />
         </div>
       </div>
@@ -92,8 +104,8 @@ export const Sidebar: React.FC<Props> = ({ image }: Props) => {
             router.reload();
           }}
           icon={faSignOutAlt}
-          color={'#BCD2E2'}
-          size={'2x'}
+          color={"#BCD2E2"}
+          size={"2x"}
         />
         {/* </Button> */}
       </div>
