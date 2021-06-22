9c2940aff91e64c757627a39631aa0c06d4300cd
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-06-22 15:07:57 +0530

    testing commit

--- web/Components/SearchBarHolder.tsx
+++ web/Components/SearchBarHolder.tsx
@@ -11,12 +11,15 @@ export const SearchBarHolder = ({ children }) => {
           className="bg-greyS w-full h-16 rounded-tl-md outline-none text-lg font-sans px-2"
           placeholder="Search"
         />
-        <FontAwesomeIcon
+        {/* <FontAwesomeIcon
           onClick={() => {}}
           className="w-16 bg-greyS p-2 h-16 rounded-tr-md border-l-4 border-white cursor-pointer"
           icon={faSearch}
           size={"xs"}
-        />
+        /> */}
+        <div className="flex justify-center items-center p-2 bg-greyS rounded-tr-md ">
+          <img src="fa-solid_search.svg" alt="search" />
+        </div>
       </div>
       <div className="h-full p-4 bg-white overflow-y-auto">{children}</div>
     </div>
--- web/pages/questions/[id].tsx
+++ web/pages/questions/[id].tsx
@@ -1,16 +1,16 @@
-import { withUrqlClient } from 'next-urql';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Comments from '../../Components/Comments';
-import MarkDown from '../../Components/MDEditor';
-import Question from '../../Components/Question';
-import { useCreateCommentMutation } from '../../generated/graphql';
-import { setAcceptedAnswer } from '../../redux/actions/questionAction';
-import { createUrqlClient } from '../../utils/createUrqlClient';
-import { GetAvatar } from '../../utils/getAvatar';
-import { useGetIntId } from '../../utils/useGetIntId';
-import { useGetQuestionFromUrl } from '../../utils/useGetQuestionFromUrl';
+import { withUrqlClient } from "next-urql";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import Comments from "../../Components/Comments";
+import MarkDown from "../../Components/MDEditor";
+import Question from "../../Components/Question";
+import { useCreateCommentMutation } from "../../generated/graphql";
+import { setAcceptedAnswer } from "../../redux/actions/questionAction";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+import { GetAvatar } from "../../utils/getAvatar";
+import { useGetIntId } from "../../utils/useGetIntId";
+import { useGetQuestionFromUrl } from "../../utils/useGetQuestionFromUrl";
 
 const DetailedQuestion = () => {
   const questionId = useGetIntId();
@@ -20,43 +20,43 @@ const DetailedQuestion = () => {
     text: string;
     html: string;
   }>({
-    text: '',
-    html: '',
+    text: "",
+    html: "",
   });
 
   const [, createComment] = useCreateCommentMutation();
 
   const addCommentHandler = async () => {
-    console.log('creating comment');
+    console.log("creating comment");
     const { error } = await createComment({ ...comment, questionId });
 
     if (!error) {
-      console.log('comment created successfully');
-      setComment({ html: '', text: '' });
+      console.log("comment created successfully");
+      setComment({ html: "", text: "" });
     } else {
-      console.log('error while creating comment: ', error);
+      console.log("error while creating comment: ", error);
     }
   };
 
   useEffect(() => {
-    console.log('comment state: ', comment);
+    console.log("comment state: ", comment);
   }, [comment]);
 
   if (fetching) {
     return (
-      <div className='m-auto text-activityBlue font-medium text-md text-center'>
+      <div className="m-auto text-activityBlue font-medium text-md text-center">
         Loading ...
       </div>
     );
   }
 
   if (error) {
-    console.log('error while fetching question', error);
+    console.log("error while fetching question", error);
   }
 
   if (data && !data.question) {
     return (
-      <div className='m-auto text-activityBlue font-medium text-md text-center'>
+      <div className="m-auto text-activityBlue font-medium text-md text-center">
         Question not found
       </div>
     );
@@ -67,27 +67,27 @@ const DetailedQuestion = () => {
   }
 
   return (
-    <div className='overflow-y-auto h-full'>
+    <div className="overflow-y-auto h-full">
       {data?.question && <Question question={data.question} />}
       {data?.question?.imageUrls?.length !== 0 && (
-        <div className='ml-12 flex justify-around'>
+        <div className="ml-12 flex justify-around">
           {data?.question?.imageUrls?.map((image) => (
-            <div className=' inline-block mr-4 border-activityBlue border-2'>
-              <GetAvatar path={image} styles='w-auto h-48' />
+            <div className=" inline-block mr-4 border-activityBlue border-2">
+              <GetAvatar path={image} styles="w-auto h-48" />
             </div>
           ))}
         </div>
       )}
-      <div className='mt-6 overflow-y-auto'>
+      <div className="mt-6 overflow-y-auto">
         <Comments pageProps />
       </div>
-      <div className='w-full lg:w-11/12 xl:w-10/12 mt-4 mb-4 m-auto '>
-        <div className='w-full h-64 overflow-y-auto'>
+      <div className="w-full lg:w-11/12 xl:w-10/12 mt-4 mb-4 m-auto ">
+        <div className="w-full h-64 overflow-y-auto">
           <MarkDown value={comment} setValue={setComment} />
         </div>
         <button
           onClick={addCommentHandler}
-          className='border-none bg-iconBlue text-blue font-semibold text-lg mt-4 mb-12 sm:mb-12 rounded-md p-2 pl-3 pr-3'
+          className="border-none bg-iconBlue text-blue font-semibold text-lg mt-4 mb-32 sm:mb-12 rounded-md p-2 pl-3 pr-3"
         >
           Add answer
         </button>
