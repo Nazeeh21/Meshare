4ac6182661d279091979d2d7388cdb55edfb81d6
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-06-25 14:14:57 +0530

    add again cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import { Question } from "./entities/Question";
 import express from "express";
 import session from "express-session";
 import path from "path";
-// import cors from "cors";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { QuestionResolver } from "./resolvers/question";
@@ -55,22 +55,22 @@ const main = async () => {
   const redis = new Redis(process.env.REDIS_URL);
 
   app.set("trust proxy", 1);
-  app.use(function(_req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+//   app.use(function(_req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+//     });
   // app.use(cors())
-  // app.use(
-  //   cors({
-  //     // origin: process.env.CORS_ORIGIN,
-  //     origin: '*',
-  //     // origin: "https://get-it-here.vercel.app",
-  //     credentials: true,
-  //   })
-  // );
+  app.use(
+    cors({
+      // origin: process.env.CORS_ORIGIN,
+      origin: '*',
+      // origin: "https://get-it-here.vercel.app",
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
