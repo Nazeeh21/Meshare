5deeb5e510991025f0f08a384945cb2150718b67
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-08-16 18:31:52 +0530

    Add accept answer

--- web/redux/actionTypes/types.ts
+++ web/redux/actionTypes/types.ts
@@ -1,3 +1,4 @@
 export const FETCH_USERS: string = "FETCH_USERS";
 export const SET_ACCEPTED_ANSWER: string = "SET_ACCEPTED_ANSWER";
 export const SET_SEARCHED: string = "SET_SEARCHED";
+export const SET_QUESTION_CREATOR_ID: string = "SET_QUESTION_CREATOR_ID";
